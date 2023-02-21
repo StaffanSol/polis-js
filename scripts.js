d3829c15c9cb244bf4849e237c0f72b46b3bc4b8
Stefan <stasoh171@studentmail.oru.se>
2023-02-21 22:22:14 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+async function getData () {
+    return await (await fetch("https://polisen.se/api/events")).json()
+}
+
+function appendTh (textContent) {
+    const thElem = document.createElement("th");
+    thElem.onclick = () =>  { sortAndRebuildTable(textContent); };
+    thElem.className = "th"
+    thElem.textContent = textContent;
+    document.getElementById("thead").appendChild(thElem)
+}
+
+function appendTr () {
+    const trElem = document.createElement("tr");
+    return document.getElementById("tbody").appendChild(trElem)
+}
+
+function appendTd (trElem, textContent) {
+    const tdElem = document.createElement("td");
+    tdElem.textContent = textContent;
+    tdElem.className = "td"
+    trElem.appendChild(tdElem)
+}
+
+function createTable(data) {
+
+    Object.keys(data[0]).forEach((keys) =>{
+        appendTh(keys)
+    })
+
+    data.forEach(row => {
+        const trElem = appendTr();
+        Object.entries(row).forEach(column => {
+            appendTd(trElem, column)
+        })
+    })
+}
+
+async function getPosts () {
+    const data = await getData()
+    createTable(data)
+}
+
+getPosts()
